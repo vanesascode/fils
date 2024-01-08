287f681bfbc855402f676cd8d5fffb2904f31293
vanesascode <vanesa.juarez.paris@gmail.com>
2024-01-08 17:05:20 +0100

    [Fix toast green modals] - Update colors to see text clearer

--- components/forms/SaveThread.tsx
+++ components/forms/SaveThread.tsx
@@ -54,11 +54,11 @@ const SaveThread = ({
       {/* SAVED & REMOVED FILSTOASTS*/}
       {addBookmarks && (
         <div className="fixed bottom-20  left-0 w-full h-full flex justify-center items-end  z-50 ">
-          <div className="bg-light-2 px-[30px] py-[20px] rounded-lg shadow text-center gap-2 text-dark-1 flex items-center">
+          <div className="bg-light-2 dark:bg-green-1 px-[30px] py-[20px] rounded-lg shadow text-center gap-2 text-dark-1 flex items-center">
             <div className="text-base-semibold">Added to your bookmarks</div>
             <Link
               href={`/profile/${currentUserId}`}
-              className="text-light-1 text-base-bold sm:text-body-bold "
+              className="text-light-1 dark:text-gray-400 text-base-bold sm:text-body-bold "
             >
               View
             </Link>
